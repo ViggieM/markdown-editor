316d0af293df3eddbc96bd6bb65698bfc015ba9b
Victor <victormovileanu@gmail.com>
2025-11-29 07:16:44 +0100

    Fix failing playwright test

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import devtoolsJson from 'vite-plugin-devtools-json';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()]
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+	test: {
+		exclude: ['**/node_modules/**', '**/e2e/**']
+	}
 });
